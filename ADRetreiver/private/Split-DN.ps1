27d2903699a2968902bc0c70fbb0be5ae1791a10
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-04-12 19:21:26 +0200

    Edit: éfinished and implemented Split-DN

--- ADRetreiver/private/Split-DN.ps1
+++ ADRetreiver/private/Split-DN.ps1
@@ -1,5 +1,12 @@
 function Split-DN ([string] $dn) {
-  $ous = $dn.Split(',') | where-object { $_ -like "OU=*" }
+  [array]$splitDN = $dn -split ','
+  [array]$domDCs = $splitDN | where-object { $_ -like "DC=*" }
+  [array]$domOUs = $splitDN | where-object { $_ -like "OU=*" }
+  [array]$domCNs = $splitDN | where-object { $_ -like "CN=*" }
 
-  return @{  }
+  return [PSCustomObject]@{
+    Name   = if ($domCNs) { $domCNs[0].Trim('CN=') } else { $null }
+    OUs    = if ($domOUs) { $domOUs.Trim('OU=') } else { $null }
+    Domain = if ($domDCs) { $domDCs.Trim('DC=') -join '.' } else { $null }
+  }
 }
