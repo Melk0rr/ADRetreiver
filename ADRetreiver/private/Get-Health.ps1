2997a1297ccfbbae099381ebdd346be81bd98f56
LALLEMENT Jaufré <J.LALLEMENT@gds.local>
2022-05-31 18:27:15 +0200

    Edit: fully implemented Get-Health

--- ADRetreiver/private/Get-Health.ps1
+++ ADRetreiver/private/Get-Health.ps1
@@ -33,33 +33,33 @@ function Get-Health {
   )
 
   BEGIN {
-    $health = 100
-    $healthFlags = @()
+    # Initializing base values
+    $health = 100; $healthFlags = @()
   }
 
   PROCESS {
     if ($Type -eq "computer") {
-      # Remove health if os is no longer supported
-      $health -= !$isSupported ? (50 + [math]::truncate((Get-Days (Get-Date $endOfSupportDate)) / 360) * 20) : 0
-      $healthFlags += !$isSupported ? "NotSupported" : "Supported"
+      $isSupported = "Ongoing", "Extended" -contains $Account.support
 
+      # Remove health if os is no longer supported
+      $health -= !$isSupported ? (50 + [math]::truncate((Get-Days (Get-Date $Account.endOfSupportDate)) / 360) * 20) : 0
+      $healthFlags += !$isSupported ? "NotSupported" : "Supported"
     }
 
-    # Health loss
     # If the password is not required immediately set health to 0
-    if ($props.PasswordNotRequired) { $health = 0; $healthFlags += "PwdNotRequired" }
+    if ($Account.passwordNotRequired) { $health = 0; $healthFlags += "PwdNotRequired" }
 
     # Remove health if the user has not logged in a while
+    $activityRule = $Account.activityRule; $pwdRule = $Account.pwdRule
     $health -= $isAdmin ? [math]::min(100, [int]$activityRule.healthLoss * 2) : [int]$activityRule.healthLoss
     $healthFlags += $isAdmin ? "Admin$($activityRule.flag)" : $activityRule.flag
 
     # Remove health if password is expired
     $health -= $isAdmin ? [math]::min(100, [int]$pwdRule.healthLoss * 2) : [int]$pwdRule.healthLoss
     $healthFlags += $isAdmin ? "Admin$($pwdRule.flag)" : $pwdRule.flag
-
-    # Add health properties
-    $props = $props | select-object *,` @{n='Health';e={ [math]::max(0, $health) }},` @{n='HealthFlags';e={ $healthFlags -join ';' }}
   }
 
-  END { return $Account }
+  END { 
+    return ($Account | select-object *,` @{n='Health';e={ [math]::max(0, $health) }},` @{n='HealthFlags';e={ $healthFlags -join ';' }})
+  }
 }
