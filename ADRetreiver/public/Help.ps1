4ea313b93f493dce4b01e5b408cc0a0b40c22287
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-10-21 11:48:13 +0200

    Add: placed help string into separated file

---
+++ ADRetreiver/public/Help.ps1
@@ -0,0 +1,18 @@
+$helpInfos = @"
+Usage:
+  Invoke-ADRetreiver (-Leads) [-Timeout]
+  Invoke-ADRetreiver -Help
+  Invoke-ADRetreiver (-Version)
+  
+Options:
+  -Help                          show this help message and exit
+  -HideBanner                    hide the banner
+  -Leads                         set leads for the script to explore
+  -MinBanners                    replace default banner with minnimal version
+  -Timeout                       set a timeout fot AD query
+  -Version                       show version and exit
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/Melk0rr/ADRetreiver
+"@
